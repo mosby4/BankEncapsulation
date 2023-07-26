f230001eccb6f489a86f408564825068de3299ce
Mosby4 <rufus.hamby@gmial.com>
2023-07-26 12:00:54 -0500

    Completed REDONE 7/26/23

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        private double Balance = 0;
+
+
+        public void Deposit(double deposit)
+        {
+            Balance += deposit;
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
+
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var MyMoney = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            Console.WriteLine();
+            var funds = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            MyMoney.Deposit(funds);
+            Console.WriteLine();
+            Console.WriteLine($"Ok, {funds} has been deposited to your account.");
+            Console.WriteLine();
+
+
+            Console.WriteLine($"Would you like to retrive your balance? y/n");
+            var response = (Console.ReadLine());
+
+            if (response == "y")
+            {
+                Console.WriteLine($"Your new balance is {MyMoney.GetBalance()}");
+                Console.WriteLine("Thank you for banking with Rufus First Federal!");
+            }
+            else
+            {
+                Console.WriteLine("Thank you for banking with Rufus First Federal!");
+            }
         }
     }
 }
